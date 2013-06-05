b72749ae9e04d166606bdbb6acc3f0abb816174f
Soham Ghosh <sohamghosh@gmail.com>
2013-06-05 20:52:08 +0530

    AppDelegate

--- Button Fun/BIDViewController.m
+++ Button Fun/BIDViewController.m
@@ -15,8 +15,20 @@
     NSLog(@"Button pressed. %@", sender.titleLabel.text);
     
     NSString *buttonText = [sender titleForState:UIControlStateNormal];
-    NSString *message = [NSString stringWithFormat:@"%@ button was pressed.", buttonText];
-    self.statusLabel.text = message;
+    
+    NSString *plainText = [NSString stringWithFormat:@"%@ button pressed.", buttonText];
+    
+    NSMutableAttributedString *styledText = [[NSMutableAttributedString alloc] initWithString:plainText];
+    
+    NSDictionary *attributes = @{NSFontAttributeName: [UIFont boldSystemFontOfSize:self.statusLabel.font.pointSize]};
+    
+    NSRange nameRange = [plainText rangeOfString:buttonText];
+    
+    [styledText setAttributes:attributes range:nameRange];
+    
+    //self.statusLabel.text = plainText;
+    
+    self.statusLabel.attributedText = styledText;
 }
 
 @end
